505828f527e42821a8bc5bbcbcbaabf11c8826d0
jpreisner <jpreisner@free.fr>
2015-10-17 00:43:15 +0200

    Adding comments client

--- src/et5/client/jaxws/ClientJaxWS.java
+++ src/et5/client/jaxws/ClientJaxWS.java
@@ -2,14 +2,27 @@ package et5.client.jaxws;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Scanner;
 
-
+/** Sites : 
+ * 
+ * http://blog.netapsys.fr/conception-dun-modele-client-serveur-avec-jax-ws/
+ * http://www.jmdoudoux.fr/java/dej/chap-service-web.htm
+ * https://www-01.ibm.com/support/knowledgecenter/SSEQTP_8.5.5///com.ibm.websphere.nd.multiplatform.doc/ae/twbs_jmsasyncresplistener.html
+ * https://docs.oracle.com/middleware/1213/wls/WSGET/jax-ws-jmstransport.htm#WSGET3638
+ * http://www.mastertheintegration.com/camel/camel-and-web-services/calling-a-jax-ws-web-service-from-camel.html
+ * 
+ *
+ */
 public class ClientJaxWS {
 	//private ConnectionFactory connectionFactory;
 
-	private void connect(){
+	private void connect() throws MalformedURLException{
+		
+        // URL wsdlLocation = new URL("http://localhost:8585/DemoService?wsdl");
+		
 		/*
 		// Creation d'un contexte JNDI
 		Context jndiContext = new InitialContext();
@@ -25,26 +38,51 @@ public class ClientJaxWS {
 		String msg;
 		
 		while(true) {
-			System.out.println("Entrer le nom d'une equipe nationale pour obtenir son parcours lors "
-					+ "de la Coupe du Monde de Football 2014 (QUIT pour arreter) :");
-			msg = scanner.nextLine();
-			
-			if (msg.equals("QUIT")) {
+			// 1. Demande ce que souhaite faire l'utilisateur
+			System.out.println("Pour obtenir le parcours d'une equipe nationale durant la Coupe du Monde 2014 : tapez 1");
+			System.out.println("Pour afficher la carte d'une ville : tapez 2");
+			System.out.println("Pour arreter le programme : tapez 'QUIT'");
+			msg = scanner.nextLine().trim();
+	
+			if (msg.toLowerCase().equals("quit"))
 				break;
+			
+			if (msg.equals("1")){
+				// 2. Demande le pays
+				System.out.println("Entrez le pays :");
+				msg = scanner.nextLine().trim();
+				
+				// 3. Ajoute les elements necessaires pour distinguer le choix de l'utilisateur
+
+			}
+			else if (msg.equals("2")){
+				// 2. Demande la ville
+				msg = scanner.nextLine().trim();
+
+				// 3. Ajoute les elements necessaires pour distinguer le choix de l'utilisateur
 			}
 			
-			// TODO : transmettre la requete http
+			// 4. Envoi le message
 			
-			System.out.println(" >> Requete envoyee,  ID = ");
-		}
+			System.out.println(">> Requete envoyee : ID=[]");
+		} 
 		scanner.close();
+		System.out.println("Bye !");
 	}
 	
 	private void readResponse(){
-		System.out.println(">> Reponse obtenue, requete correlID: --> ");
-		// Afficher le parcours
-		// Ouvrir le navigateur web par defaut avec l'URL de google maps renvoye
-		// attention : difference entre unix/windows : gerer les 2 cas
+		System.out.println(">> Reponse obtenue : ID=[]");
+		
+		// 1. Transforme l'element recu (XML) en HTML
+		
+		// 2. Ouvre l'element recu (HTML) dans le navigateur par defaut de l'utilisateur
+		try {
+			openDefaultBrowser("");
+			System.out.println("Ouverture du navigateur par defaut OK");
+		} catch (Exception e) {
+			System.err.println("Impossible d'ouvrir l'element HTML dans le navigateur par defaut. " + e.getMessage());
+		}
+		
 	}
 	
 	/**
