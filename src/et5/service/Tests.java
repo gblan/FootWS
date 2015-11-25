73d912c77e237d99ba249d50b294388e487f13d9
gblanc <guillaume.blanchard37@gmail.com>
2015-11-25 16:47:55 +0100

    add FootBallWEB SERVICE

--- src/et5/service/Tests.java
+++ src/et5/service/Tests.java
@@ -1,12 +1,15 @@
 package et5.service;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.xml.bind.JAXBException;
+import javax.xml.ws.Endpoint;
 
 import et5.service.foot.FootServiceManager;
+import et5.service.web.FootService;
 import eu.dataaccess.footballpool.Info;
 
 /**
@@ -14,8 +17,8 @@ import eu.dataaccess.footballpool.Info;
  */
 public class Tests {
 
-	public static void main(String[] args) throws AddressException,
-			MessagingException, IOException, JAXBException {
+//	public static void main(String[] args) throws AddressException,
+//			MessagingException, IOException, JAXBException {
 
 		// FootService fs = new FootService(new Info());
 		//
@@ -46,11 +49,25 @@ public class Tests {
 //		Service serv = new Service();
 //		serv.obtenirParcours(null);
 
-		FootServiceManager fs = new FootServiceManager(new Info());
-		fs.getInfoMatchById(99);
+//		FootServiceManager fs = new FootServiceManager(new Info());
+//		fs.getInfoMatchById(99);
 //		Utils.transformationXML("routeGermany.xml", "routeGermany.html", "resources/displayHTMLroute.xslt");
 //		String message = Utils.readFile("resources/example/routeGermany.html", Charset.forName("UTF-8"));
 //		Utils.sendHTMLMailUsingSMTPAppliEmail("julien.preisner@u-psud.fr","Objet Message", message);
+//	}
+	
+	public static void main(String[] args) {
+		FootService impl = new FootService();		
+		Endpoint endpoint = Endpoint.create(impl);
+		
+		endpoint.publish("http://localhost:8090/ws");
+		
+		System.err.println("Saisir (car + return) pour arreter le endpoint ");	
+		Scanner sc = new Scanner(System.in);
+		sc.next();
+		
+		endpoint.stop();
+		System.out.println("Endpoint stopp� !");
 	}
 
 }
