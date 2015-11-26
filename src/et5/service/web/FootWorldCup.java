93e0f09c5f206493e0bae5c89bf56ee9a394c245
gblanc <guillaume.blanchard37@gmail.com>
2015-11-26 14:25:28 +0100

    web service

--- src/et5/service/web/FootWorldCup.java
+++ src/et5/service/web/FootWorldCup.java
@@ -8,11 +8,6 @@ import javax.jws.WebService;
  */
 @WebService
 public class FootWorldCup {
-
-	private static final int MAIL_FORMAT_ERROR = 1;
-	private static final int MAIL_TRANSPORT_ERROR = 2;
-	private static final int CORRECT_SEND = 0;
-
 	/**
 	 * Cette methode est declaree dans le WSDL
 	 * @param name
@@ -20,27 +15,14 @@ public class FootWorldCup {
 	 */
 	@WebMethod
 	public String getRouteTeamSynchronous(String teamName){
-		// TODO return XML -> to string via JMS
-		return null;
+		FootWorldCupManager fwcm = new FootWorldCupManager();
+		return fwcm.getRouteTeamSynchronous(teamName);
 	}
 	
 	@WebMethod
 	public int getRouteTeamAsynchronous(String teamName, String mail){
-		/* ${headers.operationName} == 'obtenir_parcours_mail' */
-
-		// TODO XML -> via mail
-		// return error code
-//		if(!Utils.mailValidator(mail)){
-//			return MAIL_FORMAT_ERROR;
-//		}else{
-//			FootServiceManager fsm = new FootServiceManager(new Info());			
-//			try {
-//				Utils.sendHTMLMailUsingSMTPAppliEmail(mail, "route of XXX", fsm.obtenirParcours(teamName));
-//			} catch (MessagingException e) {
-//				return MAIL_TRANSPORT_ERROR;
-//			}
-//		}
-		return CORRECT_SEND;
+		FootWorldCupManager fwcm = new FootWorldCupManager();
+		return fwcm.getRouteTeamAsynchronous(teamName, mail);
 	}
 		
 }
