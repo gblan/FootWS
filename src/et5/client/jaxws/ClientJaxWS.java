59e152561b031fa0f0b0033046075e0806fcc466
jpreisner <jpreisner@free.fr>
2015-12-03 17:39:44 +0100

    correction ClientJaxWS

--- src/et5/client/jaxws/ClientJaxWS.java
+++ src/et5/client/jaxws/ClientJaxWS.java
@@ -12,19 +12,6 @@ import et5.client.web.FootWorldCup;
 import et5.client.web.FootWorldCupService;
 import et5.utils.UtilsIO;
 
-/**
- * Sites :
- * 
- * http://blog.netapsys.fr/conception-dun-modele-client-serveur-avec-jax-ws/
- * http://www.jmdoudoux.fr/java/dej/chap-service-web.htm
- * https://www-01.ibm.com/support/knowledgecenter/SSEQTP_8.5.5///com.ibm.
- * websphere.nd.multiplatform.doc/ae/twbs_jmsasyncresplistener.html
- * https://docs.oracle.com/middleware/1213/wls/WSGET/jax-ws-jmstransport.htm#
- * WSGET3638
- * http://www.mastertheintegration.com/camel/camel-and-web-services/calling-a-
- * jax-ws-web-service-from-camel.html
- * 
- */
 public class ClientJaxWS {
 	protected static final String RESPONSE_TYPE = "responseType";
 	private static final int MAIL_FORMAT_ERROR = 1;
@@ -39,34 +26,40 @@ public class ClientJaxWS {
 		/* Appel des méthodes du web service*/
 		FootWorldCupService fwcs;
 		FootWorldCup port = null;
+		
+		// Tentative de connexion au WS 'FootWorldCupService'
 		try{
 			fwcs = new FootWorldCupService();
 			port = fwcs.getFootWorldCupPort();
-		}catch(WebServiceException e){
+		}
+		catch(WebServiceException e){
 			System.err.println("Impossible de se connecter au web service FootWorldCupService : "+e.getMessage());
 			System.exit(-1);
 		}
+		
+		System.out.println("Connexion au Web Service 'FootWorldCupService' réussie.");
+		System.out.println("==================================================================================================");
+		System.out.println("Web service permettant d'obtenir le parcours d'une equipe nationale durant la Coupe du Monde 2014");
+		System.out.println("==================================================================================================");
+		
 		while (true) {
-			// 1. Demande ce que souhaite faire l'utilisateur
-			System.out.println(
-					"Web service permettant d'obtenir le parcours d'une equipe nationale durant la Coupe du Monde 2014");
+			// 1. Demande le pays
+			System.out.println("Saisir un pays (en Anglais) :");
+			country = scanner.nextLine().trim().toLowerCase();
 
-			// 2. Demande le pays
-			System.out.println("Entrez le pays (en Anglais):");
-			country = scanner.nextLine().trim();
-
-			// 3. Ajoute les elements necessaires pour distinguer le choix de
-			// l'utilisateur
-			System.out.println("Veuillez maintenant choisir le mode de réception");
-			System.out.println("Pour le mode synchrone (réception par file JMS puis affichage navigateur), tapez 1");
-			System.out.println("Pour le mode asynchrone (réception par e-mail), tapez 2");
+			// 2. Ajoute les elements necessaires pour distinguer le choix de l'utilisateur
+			System.out.println("Veuillez maintenant choisir le mode de réception :");
+			System.out.println("   Pour le mode synchrone (réception par file JMS puis affichage navigateur) \t Tapez 1");
+			System.out.println("   Pour le mode asynchrone (réception par e-mail) \t\t Tapez 2");
 			int choix = Integer.parseInt(scanner.nextLine().trim());
+			
+			// Affiche le resultat dans le navigateur web (page HTML)
 			if (choix == 1) {
-				
-				/* afficher l'xml dans une page web avec le xslt */
 				ClientJaxWS.transformAndDisplayRouteTeam(port.getRouteTeamSynchronous(country),country);
 			
-			} else if (choix == 2) {
+			} 
+			// Envoie le resultat par mail
+			else if (choix == 2) {
 				System.out.println("Veuillez saisir votre e-mail :");
 				String mail = scanner.nextLine().trim();
 				
@@ -87,14 +80,13 @@ public class ClientJaxWS {
 						break;
 				}
 
-			} else {
-				System.out.println("Erreur dans la saisie!");
-
+			} 
+			else {
+				System.out.println("Erreur dans la saisie! Veuillez saisir 1 ou 2.");
 			}
 
 			System.out.println("Pour arreter le programme : tapez 'quit'");
-			System.out.println("Pour obtenir un nouveau parcours d'équipe, saisir : tapez 'Entrer'");
-			msg = scanner.nextLine().trim();
+			msg = scanner.nextLine().trim().toLowerCase();
 			if (msg.equals("quit")) {
 				break;
 			}
@@ -116,7 +108,7 @@ public class ClientJaxWS {
 	}
 	
 	/**
-	 * Open the user default browser and go to the url passed as a parameter
+	 * Open the user default browser and go to the url of the file passed as a parameter
 	 * Tested on Windows 7, 8, Ubuntu, MacOS
 	 * https://openclassrooms.com/forum/sujet/ouvrir-une-page-web-54152
 	 * 
