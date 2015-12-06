502d86184338df1bdf97b09b342daffbdc02a540
gblanc <guillaume.blanchard37@gmail.com>
2015-12-06 12:01:59 +0100

    update main for tests

--- src/et5/service/Tests.java
+++ src/et5/service/Tests.java
@@ -1,57 +1,43 @@
 package et5.service;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Scanner;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.xml.bind.JAXBException;
 import javax.xml.ws.Endpoint;
 
+import et5.service.foot.FootServiceManager;
+import et5.service.route.Route;
 import et5.service.web.FootWorldCup;
+import et5.utils.UtilsIO;
+import et5.utils.UtilsMail;
+import eu.dataaccess.footballpool.Info;
 
 /**
  * CLasse de tests
  */
 public class Tests {
 
-//	public static void main(String[] args) throws IOException{
-//			MessagingException, IOException, JAXBException {
-
-		// FootService fs = new FootService(new Info());
-		//
-		// String country = "Germany";
-		// List<Integer> idPays = fs.getAllMatchesTeam(country);
-		// System.out.println("Route of '"+country+"' : \n");
-		// for (int id : idPays) {
-		// System.out.println(fs.getInfoMatchById(id));
-		// }
-		//
-		// System.out.println(fs.getCountryInformation(country));
-		//
-		// ConsoleHTTPClient.openDefaultBrowser("https://openclassrooms.com/forum/sujet/ouvrir-une-page-web-54152");
-
-//		CountryService cs = new CountryService();
-//		System.out.println(cs.getCountryInformation("Italy"));
-		// FootService fs = new FootService(new Info());
-		//
-		// String country = "Germany";
-		// List<Integer> idPays = fs.getAllMatchesTeam("Germany");
-		// System.out.println("Route of '" + country + "' : \n");
-		// for (int id : idPays) {
-		// Match x = fs.getInfoMatchById(id);
-		// System.out.println(x);
-		// }
-		//
-		// System.out.println(fs.getCountryRoute(country));
-//		Service serv = new Service();
-//		serv.obtenirParcours(null);
-
-//		FootServiceManager fs = new FootServiceManager(new Info());
-//		fs.getInfoMatchById(99);
-//		String route = Utils.fileToString("resources/example/routeGermany.xml");
+//	public static void main(String[] args) throws IOException, JAXBException, AddressException, MessagingException{
+////			MessagingException, IOException, JAXBException {
+//
+//		 FootServiceManager fs = new FootServiceManager(new Info());
+//		//
+//		 String country = "Germany";
+//		Route r = fs.getCountryRoute(country);
+//		System.out.println(UtilsIO.marshalToString("et5.service.route", r));
+//
+//
+//		String route = UtilsIO.fileToString("resources/example/routeGermany.xml");
 //		System.out.println(route);
 //		
-//		Utils.transformationXMLFromString(route, "resources/example/routeGermany.html", "resources/displayHTMLroute.xslt");
-//		Utils.transformationXMLFromFile("resources/example/routeGermany.xml", "resources/example/routeGermany.html", "resources/displayHTMLroute.xslt");
-//		String message = Utils.readFile("resources/example/routeGermany.html", Charset.forName("UTF-8"));
-//		Utils.sendHTMLMailUsingSMTPAppliEmail("julien.preisner@u-psud.fr","Objet Message", message);
+//		UtilsIO.transformXMLFileintoHTMLFile("resources/example/routeGermany.xml", "resources/example/routeGermany.html", "resources/displayHTMLroute.xslt");
+//		String message = UtilsIO.readFile("resources/example/routeGermany.html", Charset.forName("UTF-8"));
+//		UtilsMail.sendHTMLMailUsingSMTPAppliEmail("julien.preisner@u-psud.fr","Objet Message", message);
 //	}
 	
 	/* pour lancer le ws local (route) */
